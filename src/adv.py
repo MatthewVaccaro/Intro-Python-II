66512c6d122c32e206f1c828c5377a512ef8e951
Matt Vaccaro <mattvaccaro@Matts-MBP.hsd1.tn.comcast.net>
2020-08-11 20:16:58 -0500

    complete

--- src/adv.py
+++ src/adv.py
@@ -1,24 +1,18 @@
 from room import Room
-
+from player import Player
+import sys
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside': Room("Outside Cave Entrance","North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer': Room("Foyer", """Dim light filters in from the south. Dusty passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow': Room("Narrow Passage", """The narrow passage bends here from west to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."""),
 }
 
 
@@ -37,15 +31,25 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
+cardinalDirs = ("n", "e", "s", "w")
+player = Player("Terminal Lad",room['outside'])
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+def chooseDirection():
+    direction = input('choose dirrection [N]orth, [E]ast, [S]outh, [W]est -->')
+    return direction
+
+
+while True:
+
+    print(f"\n---------------------------------- ⚔️ {player.name} ⚔️ ----------------------------------")
+
+    print(f"\n{player.currentRoom}\n")
+
+    command = chooseDirection()
+    if command == 'q':
+        sys.exit("Thanks for playing")
+    elif command not in cardinalDirs:
+        print("Use a single letter ")
+    else:
+        player.move(f'{command}_to')
+    
--- src/player.py
+++ src/player.py
@@ -1,2 +1,17 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, name, currentRoom):
+        self.name = name
+        self.currentRoom = currentRoom
+
+    def move(self, direction):
+        if hasattr(self.currentRoom, f"{direction}"):
+            self.currentRoom = getattr(self.currentRoom, f'{direction}')
+        else:
+            print("\n\nYou cannot walk that way.\n\n")
+
+    def __str__(self):
+        f"Ayy, {self.name} you're in {self.currentRoom}"
+
